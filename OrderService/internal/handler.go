4e5afbd7177bc967e22002b762f00c229b51d4d3
Ezgi Tokgoz <ezgitokgoz16@gmail.com>
2025-07-29 17:59:58 +0300

    Add order deletion endpoint and service method
    - Implemented DeleteOrderByID method in the service layer to delete an order by its ObjectID.
    - Added route and handler for deleting an order via DELETE /order/:id endpoint.
    - Included error handling for invalid or missing IDs.

--- OrderService/internal/handler.go
+++ OrderService/internal/handler.go
@@ -1,1 +1,35 @@
 package internal
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Handler struct {
+	service *Service
+}
+
+func NewHandler(e *echo.Echo, service *Service) {
+	handler := &Handler{service: service}
+
+	g := e.Group("/order")
+	//g.GET("/:id", handler.GetByID)
+	//g.POST("/", handler.Create)
+	//g.PUT("/:id", handler.Update)
+	g.DELETE("/:id", handler.Delete)
+	//g.GET("/list", handler.GetList)
+}
+
+func (h *Handler) Delete(c echo.Context) error {
+	id := c.Param("id")
+
+	err := h.service.DeleteOrderByID(id)
+	if err != nil {
+		return c.JSON(http.StatusNotFound, echo.Map{
+			"message": "Order not found",
+		})
+	}
+
+	return c.NoContent(http.StatusNoContent)
+}
