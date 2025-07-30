b0eceb1ba2a79a03d744f0c316aa6da112fab0c3
Berru Hanedar <berruhanedar@icloud.com>
2025-07-30 14:33:02 +0300

    update

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"tesodev-korpes/CustomerService/cmd"
+	customercmd "tesodev-korpes/CustomerService/cmd"
 	ordercmd "tesodev-korpes/OrderService/cmd"
 	_ "tesodev-korpes/docs"
 	"tesodev-korpes/pkg"
@@ -14,7 +14,7 @@ import (
 )
 
 func main() {
-	//todo : what is dev,qa,prod ? explain why we are using them in the lecture
+	// env: dev, qa, prod
 	dbConf := config.GetDBConfig("dev")
 
 	client, err := pkg.GetMongoClient(dbConf.MongoDuration, dbConf.MongoClientURI)
@@ -23,6 +23,7 @@ func main() {
 	}
 	fmt.Println("connecting db")
 
+	// === CUSTOMER SERVICE ===
 	customerEcho := echo.New()
 	customerEcho.Use(middleware.CorrelationIdMiddleware())
 	customerEcho.Use(middleware.LoggingMiddleware)
@@ -30,35 +31,13 @@ func main() {
 	customerEcho.Use(middleware.ErrorHandler())
 	customerEcho.GET("/swagger/*", echoSwagger.WrapHandler)
 
+	// === ORDER SERVICE ===
 	orderEcho := echo.New()
+	orderEcho.GET("/swagger/*", echoSwagger.WrapHandler)
 
-	/*e.GET("/panic", func(c echo.Context) error {
-		panic("test panic")
-	})*/
+	go func() {
+		customercmd.BootCustomerService(client, customerEcho)
+	}()
 
-	customerEcho.GET("/swagger/*", echoSwagger.WrapHandler)
-	// http://localhost:8001/swagger/index.html#/
-
-	switch input {
-	case "customer":
-		cmd.BootCustomerService(client, customerEcho)
-	case "order":
-
-		ordercmd.BootOrderService(client, orderEcho)
-
-	default:
-		panic("Invalid input. Use 'customer', 'order', or 'both'.")
-	}
-
-	//challenge : after you create a func boot order service, manage somehow to run specific project
-	//description : when you give an input here it should look that input and boot THAT specific project
-	//if the input says "both" it should
-
-	//PS : do not forget to create and call a different column for order service and do not forget to boot order service
-	//from another port different than customer service
-
-	//orderCol, err := pkg.GetMongoCollection(client, "tesodev", "order")
-	//if err != nil {
-	//	panic(err)
-
+	ordercmd.BootOrderService(client, orderEcho)
 }
