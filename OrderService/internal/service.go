423cba35b9f9caf6c754f4689f302d7ba1c3d963
ecemtan <ecem.berivan.ali@hotmail.com>
2025-07-31 13:39:25 +0300

    http client & create and getById

--- OrderService/internal/service.go
+++ OrderService/internal/service.go
@@ -1,6 +1,17 @@
 package internal
 
-import "errors"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+
+	// "go.mongodb.org/mongo-driver/mongo/options"
+	"tesodev-korpes/OrderService/internal/types"
+	// "time"
+)
 
 type Service struct {
 	repo *Repository
@@ -9,6 +20,17 @@ type Service struct {
 func NewService(repo *Repository) *Service {
 	return &Service{repo: repo}
 }
+func (s *Service) GetByID(ctx context.Context, id string) (*types.OrderResponseModel, error) {
+	order, err := s.repo.GetByID(ctx, id)
+	if err != nil {
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			return nil, fmt.Errorf("order not found for ID: %s", id)
+		}
+		return nil, fmt.Errorf("failed to get order: %w", err)
+	}
+
+	return ToOrderResponse(order), nil
+}
 
 func (s *Service) DeleteOrderByID(id string) error {
 	if id == "" {
@@ -16,4 +38,11 @@ func (s *Service) DeleteOrderByID(id string) error {
 	}
 
 	return s.repo.Delete(id)
+}
+func (s *Service) Create(ctx context.Context, order *types.Order) (string, error) {
+	order.CreatedAt = time.Now()
+	order.UpdatedAt = time.Now()
+	order.IsActive = true
+
+	return s.repo.Create(ctx, order)
 }
