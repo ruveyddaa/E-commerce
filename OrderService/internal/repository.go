7caa183e38c9d598ed872d1b04253bd0fe35dacb
Ezgi Tokgoz <ezgitokgoz16@gmail.com>
2025-08-03 14:30:17 +0300

    Updated the cancel function to support UUID

--- OrderService/internal/repository.go
+++ OrderService/internal/repository.go
@@ -44,11 +44,6 @@ func (r *Repository) Cancel(id string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	objectID, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		return err
-	}
-
 	update := bson.M{
 		"$set": bson.M{
 			"status":     types.OrderCanceled,
@@ -57,7 +52,7 @@ func (r *Repository) Cancel(id string) error {
 		},
 	}
 
-	filter := bson.M{"_id": objectID}
+	filter := bson.M{"_id": id}
 	res, err := r.collection.UpdateOne(ctx, filter, update)
 	if err != nil {
 		return err
