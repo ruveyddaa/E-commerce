20d01dfa894642f44691df8982a867d6604d6e63
Berru Hanedar <berruhanedar@icloud.com>
2025-08-06 11:04:18 +0300

    include Token field in Customer model to hold JWT token

--- CustomerService/internal/types/model.go
+++ CustomerService/internal/types/model.go
@@ -15,7 +15,7 @@ type Customer struct {
 	CreatedAt time.Time `bson:"created_at" json:"created_at"`
 	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
 	IsActive  bool      `bson:"is_active" json:"is_active"`
-	//Token     string    `bson:"token" json:"token"`
+	Token     string    `bson:"token" json:"token"`
 }
 
 type Address struct {
