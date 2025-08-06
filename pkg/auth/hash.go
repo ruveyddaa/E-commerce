89ba84f6196305f6967fdd1ec3b65edd1a130d52
Berru Hanedar <berruhanedar@icloud.com>
2025-08-06 11:00:30 +0300

    organize password hashing functions into auth package

---
+++ pkg/auth/hash.go
@@ -0,0 +1,23 @@
+package auth
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hashed), err
+}
+
+func VerifyPassword(plainPassword string, hashedPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainPassword))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
