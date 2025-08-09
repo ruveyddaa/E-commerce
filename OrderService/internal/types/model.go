2089c1a80e47488947c84ded2265e7bdbb492be1
Berru Hanedar <berruhanedar@icloud.com>
2025-08-10 02:33:39 +0300

    remove json tags

--- OrderService/internal/types/model.go
+++ OrderService/internal/types/model.go
@@ -3,33 +3,33 @@ package types
 import "time"
 
 type Order struct {
-	Id              string      `bson:"_id,omitempty" json:"id"`
-	CustomerId      string      `bson:"customer_id" json:"customer_id"`
-	Items           []OrderItem `bson:"items" json:"items"`
-	ShippingAddress Address     `bson:"shipping_address" json:"shipping_address"`
-	BillingAddress  Address     `bson:"billing_address" json:"billing_address"`
-	TotalPrice      float64     `bson:"total_price" json:"total_price"`
-	Status          string      `bson:"status" json:"status"`
-	CreatedAt       time.Time   `bson:"created_at" json:"created_at"`
-	UpdatedAt       time.Time   `bson:"updated_at" json:"updated_at"`
-	IsDelete        bool        `bson:"is_delete" json:"is_delete"`
+	Id              string      `bson:"_id,omitempty"`
+	CustomerId      string      `bson:"customer_id"`
+	Items           []OrderItem `bson:"items"`
+	ShippingAddress Address     `bson:"shipping_address"`
+	BillingAddress  Address     `bson:"billing_address"`
+	TotalPrice      float64     `bson:"total_price"`
+	Status          string      `bson:"status"`
+	CreatedAt       time.Time   `bson:"created_at"`
+	UpdatedAt       time.Time   `bson:"updated_at"`
+	IsDelete        bool        `bson:"is_delete"`
 }
 
 type OrderItem struct {
-	ProductId   string  `bson:"product_id" json:"product_id"`
-	ProductName string  `bson:"product_name" json:"product_name"`
-	Quantity    int     `bson:"quantity" json:"quantity"`
-	UnitPrice   float64 `bson:"unit_price" json:"unit_price"`
+	ProductId   string  `bson:"product_id"`
+	ProductName string  `bson:"product_name"`
+	Quantity    int     `bson:"quantity"`
+	UnitPrice   float64 `bson:"unit_price"`
 }
 
 type Address struct {
-	Id      string `bson:"address_id,omitempty" json:"address_id"`
-	City    string `bson:"city" json:"city"`
-	State   string `bson:"state" json:"state"`
-	ZipCode string `bson:"zip_code" json:"zip_code"`
+	Id      string `bson:"address_id,omitempty"`
+	City    string `bson:"city"`
+	State   string `bson:"state"`
+	ZipCode string `bson:"zip_code"`
 }
 
 type Phone struct {
-	Id          string `json:"phone_id" bson:"phone_id,omitempty"`
-	PhoneNumber int    `json:"phone_number" bson:"phone_number"`
+	Id          string `bson:"phone_id,omitempty"`
+	PhoneNumber int    `bson:"phone_number"`
 }
