5496a70dfbd70cfa2d0ca4e48f09c038c2548af6
ebrardd <135167058+ebrardd@users.noreply.github.com>
2025-08-11 01:32:53 +0300

    loginfowithcorrelation replace

--- pkg/customError/errors.go
+++ pkg/customError/errors.go
@@ -28,7 +28,7 @@ func newAppError(key ErrorKey, err error, args ...interface{}) *AppError {
 	details, ok := ErrorDefinitions[key]
 	if !ok {
 		log.Errorf("FATAL: Undefined error key requested: %s", key)
-		details = ErrorDefinitions[InternalServerError] 
+		details = ErrorDefinitions[InternalServerError]
 	}
 
 	msg := details.Message
@@ -75,4 +75,13 @@ func NewInternal(key ErrorKey, err error) *AppError {
 
 func NewValidation(key ErrorKey) *AppError {
 	return newAppError(key, nil)
+}
+
+func LogErrorWithCorrelation(err error, correlationID string) {
+
+	log.Printf("[CorrelationID: %s] %s", correlationID, err.Error())
+
+}
+func LogInfoWithCorrelation(message string, correlationID string) {
+	log.Printf("[CorrelationID: %s] %s", correlationID, message)
 }
--- pkg/utils.go
+++ pkg/utils.go
@@ -2,17 +2,7 @@ package pkg
 
 import (
 	"github.com/google/uuid"
-	"log"
 )
-
-func LogErrorWithCorrelation(err error, correlationID string) {
-
-	log.Printf("[CorrelationID: %s] %s", correlationID, err.Error())
-
-}
-func LogInfoWithCorrelation(message string, correlationID string) {
-	log.Printf("[CorrelationID: %s] %s", correlationID, message)
-}
 
 func IsValidUUID(id string) bool {
 	_, err := uuid.Parse(id)
