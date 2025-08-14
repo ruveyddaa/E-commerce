9f30c88b902f6abb3252a93d55ae47cf6993b949
ebrardd <135167058+ebrardd@users.noreply.github.com>
2025-08-14 14:49:41 +0300

    func added

--- pkg/middleware/authentication.go
+++ pkg/middleware/authentication.go
@@ -1,16 +1,20 @@
 package middleware
 
 import (
+	"context"
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"strings"
 	"tesodev-korpes/pkg/auth"
 	"tesodev-korpes/pkg/customError"
-
-	"github.com/labstack/echo/v4"
+	"time"
 )
 
 type SkipperFunc func(c echo.Context) bool
 
-func Authentication(skipper SkipperFunc) echo.MiddlewareFunc {
+func Authentication(mongoClient *mongo.Client, skipper SkipperFunc) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			if skipper != nil && skipper(c) {
@@ -31,7 +35,35 @@ func Authentication(skipper SkipperFunc) echo.MiddlewareFunc {
 			}
 
 			c.Set("userID", claims.ID)
+			role, err := getUserRoleFromMongo(mongoClient, claims.ID)
+			if err != nil {
+				return customError.NewUnauthorized("")
+			}
+			c.Set("role", role)
 			return next(c)
 		}
 	}
+}
+
+func getUserRoleFromMongo(mongoClient *mongo.Client, userID string) (string, error) {
+	col := mongoClient.Database("tesodev").Collection("customer")
+
+	var doc struct {
+		System struct {
+			Role string `bson:"role"`
+		} `bson:"system"`
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := col.FindOne(ctx, bson.M{"_id": userID}).Decode(&doc); err != nil {
+		return "", err
+	}
+
+	if doc.System.Role == "" {
+		return "", fmt.Errorf("role not found for user %s", userID)
+	}
+
+	return doc.System.Role, nil
 }
