e674e004abbf9154fb38394a0e4e98dd399cfcd6
ebrardd <135167058+ebrardd@users.noreply.github.com>
2025-08-14 14:49:05 +0300

    typo

--- CustomerService/internal/types/type.go
+++ CustomerService/internal/types/type.go
@@ -5,35 +5,40 @@ import (
 )
 
 type CreateCustomerRequestModel struct {
-	FirstName string    `json:"first_name" validate:"required,min=2,max=50"`
-	LastName  string    `json:"last_name" validate:"required,min=2,max=50"`
-	Email     string    `json:"email" validate:"required,email"`
-	Phone     []Phone   `json:"phone" validate:"required,dive"`
-	Address   []Address `json:"address" validate:"required,dive"`
-	Password  string    `json:"password" validate:"required"`
+	FirstName    string       `json:"first_name" validate:"required,min=2,max=50"`
+	LastName     string       `json:"last_name" validate:"required,min=2,max=50"`
+	Email        string       `json:"email" validate:"required,email"`
+	Phone        []Phone      `json:"phone" validate:"required,dive"`
+	Address      []Address    `json:"address" validate:"required,dive"`
+	Password     string       `json:"password" validate:"required"`
+	System       System       `json:"system" validate:"required,dive"`
+	Subscription Subscription `json:"subscription" validate:"required,dive"`
 }
 
 type UpdateCustomerRequestModel struct {
-	FirstName string    `json:"first_name,omitempty" validate:"omitempty,min=2,max=50"`
-	LastName  string    `json:"last_name,omitempty" validate:"omitempty,min=2,max=50"`
-	Email     string    `json:"email" validate:"required,email"`
-	Phone     []Phone   `json:"phone,omitempty" validate:"omitempty,dive"`
-	Address   []Address `json:"address,omitempty" validate:"omitempty,dive"`
-	Password  string    `json:"password,omitempty" validate:"omitempty"`
-	IsActive  bool      `json:"is_active,omitempty"`
+	FirstName    string       `json:"first_name,omitempty" validate:"omitempty,min=2,max=50"`
+	LastName     string       `json:"last_name,omitempty" validate:"omitempty,min=2,max=50"`
+	Email        string       `json:"email" validate:"required,email"`
+	Phone        []Phone      `json:"phone,omitempty" validate:"omitempty,dive"`
+	Address      []Address    `json:"address,omitempty" validate:"omitempty,dive"`
+	Password     string       `json:"password,omitempty" validate:"omitempty"`
+	IsActive     bool         `json:"is_active,omitempty"`
+	System       System       `json:"system,omitempty" validate:"omitempty,dive"`
+	Subscription Subscription `json:"subscription,omitempty" validate:"omitempty,dive"`
 }
 
 type CustomerResponseModel struct {
-	ID        string    `json:"id"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Email     string    `json:"email"`
-	Phone     []Phone   `json:"phone"`
-	Address   []Address `json:"address"`
-	IsActive  bool      `json:"is_active"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
-	Role      string    `json:"role"`
+	ID           string       `json:"id"`
+	FirstName    string       `json:"first_name"`
+	LastName     string       `json:"last_name"`
+	Email        string       `json:"email"`
+	Phone        []Phone      `json:"phone"`
+	Address      []Address    `json:"address"`
+	IsActive     bool         `json:"is_active"`
+	CreatedAt    time.Time    `json:"created_at"`
+	UpdatedAt    time.Time    `json:"updated_at"`
+	System       System       `json:"system"`
+	Subscription Subscription `json:"subscription"`
 }
 
 type Pagination struct {
